0dc3f0226e531dd6d007203277e6b8117bbfafcd
IBEGHOUCHENE <ibeghouchene.nadir@gmail.com>
2021-05-12 16:30:06 +0100

    Make use webpack encore and lang attribute to html tag

---
+++ assets/app.js
@@ -0,0 +1,25 @@
+/*
+ * Welcome to your app's main JavaScript file!
+ *
+ * We recommend including the built version of this JavaScript file
+ * (and its CSS file) in your base layout (base.html.twig).
+ */
+
+// any CSS you import will output into a single css file (app.scss in this case)
+import './scss/app.scss';
+
+//import $ from 'jquery';
+
+// start the Stimulus application
+import './bootstrap';
+
+/*$("#clickMe").click(function send(){
+    console.log('clicked');
+});*/
+
+/*$(document).ready(function() {
+    $('[data-toggle="popover"]').popover();
+});*/
+
+/*
+console.log('hello');*/
