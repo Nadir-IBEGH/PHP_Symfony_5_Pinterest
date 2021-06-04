412b32c8690181260c8d4566b9ad241e14f9e198
IBEGHOUCHENE <ibeghouchene.nadir@gmail.com>
2021-06-04 19:21:08 +0100

    Update welcome message

--- src/Security/LoginFormAuthentificatorAuthenticator.php
+++ src/Security/LoginFormAuthentificatorAuthenticator.php
@@ -103,7 +103,7 @@ class LoginFormAuthentificatorAuthenticator extends AbstractFormLoginAuthenticat
 
     public function onAuthenticationSuccess(Request $request, TokenInterface $token, string $providerKey)
     {
-        $request->getSession()->getFlashBag()->add('success',"Logged in succesfully!");
+        $request->getSession()->getFlashBag()->add('success',"Welcome " .$token->getUser()->getFullName(). '!');
 
         if ($targetPath = $this->getTargetPath($request->getSession(), $providerKey)) {
             return new RedirectResponse($targetPath);
